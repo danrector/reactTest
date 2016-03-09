4bcdec5f6ebdf37c3ae444ed3013ea311616d155
Daniel Rector <rector.danielk@gmail.com>
2016-03-09 17:36:47 -0500

    added js content, hello world render

--- helloworld.js
+++ helloworld.js
@@ -1,2 +1,13 @@
 //hello world react test page
 //this is a comment :)
+
+var Hello = React.createClass({
+  render: function() {
+    return <div>Hello {this.props.name}</div>;
+  }
+});
+
+ReactDOM.render(
+  <Hello name="World" />,
+  document.getElementById('container')
+);
